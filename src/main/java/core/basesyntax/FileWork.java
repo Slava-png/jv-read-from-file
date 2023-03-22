76d24c54e2840b570c4be4c5ae62d1df80acacba
Slava-png <pylyp.sviatoslav@gmail.com>
2023-03-22 22:55:52 +0100

    create reading file line-by-line

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,43 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final char SPECIAL_SYMBOL = 'w';
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder text = new StringBuilder();
+        File file = new File(fileName);
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                String[] words = line.toLowerCase().split(" ");
+
+                for (String word : words) {
+                    if (word.charAt(0) == SPECIAL_SYMBOL) {
+                        text.append(word).append(" ");
+                    }
+                }
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Test failed! You should returned empty array.", e);
+        }
+
+        if (text.toString().isEmpty()) {
+            return new String[] {};
+        }
+        String withoutPunctuation = text.toString().replaceAll("\\p{P}", "");
+        String[] words = withoutPunctuation.split(" ");
+
+        Arrays.sort(words);
+        return words;
     }
 }
